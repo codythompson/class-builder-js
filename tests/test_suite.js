285d93a2add05a594ecd42077f2e3e65a7d10b97
Cody Thompson <cody.thompson22@gmail.com>
2017-09-17 14:11:29 -0700

    Added tests for using class_builder on existing objects.

--- tests/test_suite.js
+++ tests/test_suite.js
@@ -66,5 +66,84 @@ var testSuite = {
         };
         var expErr = '[C][constructor] test3 is a required argument.';
         assertError(funcB, expErr, 'Expceted the constructor to throw ' + expErr);
+    },
+
+
+    /*
+     * The followin mirror the tests above,
+     * but test for the case where used with
+     * an existing object
+     */
+    "ClassBuilder field function with existing class": function () {
+        var cb = ClassBuilder;
+        cb.new('C');
+        cb.field('test1');
+        cb.field('test2');
+        cb.field('test3');
+        var init = cb.build();
+
+        var C = function (args) {
+            init.call(this, args)
+        };
+        var c = new C({
+            test1: true,
+            test2: true,
+            test3: true
+        });
+
+        assert(c.test1, 'field test1 not defined');
+        assert(c.test2, 'field test2 not defined');
+        assert(c.test3, 'field test3 not defined');
+    },
+
+    "ClassBuilder default function with existing class": function () {
+        var cb = ClassBuilder;
+        cb.new('C');
+        cb.default('test1', true);
+        cb.default('test3', false);
+        cb.field('test2');
+        cb.field('test3');
+        var init = cb.build();
+
+        var C = function (args) {
+            init.call(this, args)
+        };
+        var c = new C({
+            test2: true,
+            test3: true,
+        });
+
+        assertNotExists(c.test1, 'field test1 should not exist');
+        assertExists(c.test2, 'field test3 not defined');
+        assertExists(c.test3, 'field test3 not defined');
+    },
+    "ClassBuilder required function with existing class": function () {
+        var cb = ClassBuilder;
+        cb.new('C');
+        cb.default('test1', 0);
+        cb.require('test1');
+        cb.require('test2');
+        cb.require('test3');
+        var init = cb.build();
+
+        var C = function (args) {
+            init.call(this, args)
+        };
+
+        var funcA = function () {
+            var c = new C({
+                test2: '',
+                test3: 0
+            });
+        };
+        assertNoError(funcA, 'No error should be thrown, all required args provided.');
+
+        var funcB = function () {
+            var c = new C({
+                test2: ''
+            });
+        };
+        var expErr = '[C][constructor] test3 is a required argument.';
+        assertError(funcB, expErr, 'Expceted the constructor to throw ' + expErr);
     }
 };
